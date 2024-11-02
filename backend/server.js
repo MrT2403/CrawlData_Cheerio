844e936b73246fcfe24ecf4e4db6673303d2f1cf
MrT2403 <minhtri240301@gmail.com>
2024-11-02 08:54:13 +0700

    fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,17 @@ import cors from "cors";
 
 dotenv.config();
 
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://crawl-data-cheerio.vercel.app"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use("/api/v1", routes);
 
 mongoose
